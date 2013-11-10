4a0f6b3ca86d87e2b3cede23b4fca0dd0d821a82
Camilo Lopes <camilo@camilolopes.com.br>
2013-11-10 19:12:56 -0200

    first commit

---
+++ isaude-core/src/main/java/org/isaude/core/App.java
@@ -0,0 +1,13 @@
+package org.isaude.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
