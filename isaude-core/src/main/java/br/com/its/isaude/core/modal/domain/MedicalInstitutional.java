7acec0d6a4f407119d6ffddf1ee10f216b8a482c
Camilo Lopes <camilo@camilolopes.com.br>
2013-11-23 19:05:47 -0200

    add medical institutional and unit test

--- isaude-core/src/main/java/br/com/its/isaude/core/modal/domain/MedicalInstitutional.java
+++ isaude-core/src/main/java/br/com/its/isaude/core/modal/domain/MedicalInstitutional.java
@@ -109,6 +109,51 @@ public class MedicalInstitutional implements java.io.Serializable {
 	public void setMedicos(Set<Doctor> medicos) {
 		this.medicos = medicos;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((cnpj == null) ? 0 : cnpj.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result
+				+ ((nomeFantasia == null) ? 0 : nomeFantasia.hashCode());
+		result = prime * result
+				+ ((razaoSocial == null) ? 0 : razaoSocial.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof MedicalInstitutional))
+			return false;
+		MedicalInstitutional other = (MedicalInstitutional) obj;
+		if (cnpj == null) {
+			if (other.cnpj != null)
+				return false;
+		} else if (!cnpj.equals(other.cnpj))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (nomeFantasia == null) {
+			if (other.nomeFantasia != null)
+				return false;
+		} else if (!nomeFantasia.equals(other.nomeFantasia))
+			return false;
+		if (razaoSocial == null) {
+			if (other.razaoSocial != null)
+				return false;
+		} else if (!razaoSocial.equals(other.razaoSocial))
+			return false;
+		return true;
+	}
 	
 
 }
