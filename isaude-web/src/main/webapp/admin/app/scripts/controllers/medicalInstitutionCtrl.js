12b3833740a17bfa98d3499b3b80cb8e8deda6bf
Camilo Lopes <camilo@camilolopes.com.br>
2013-11-26 22:56:48 -0200

    rename medicalInstitution js

---
+++ isaude-web/src/main/webapp/admin/app/scripts/controllers/medicalInstitutionCtrl.js
@@ -0,0 +1,58 @@
+'use strict';
+
+angular.module('iSaudeAdminApp')
+  .controller('MedicalinstitutionCtrl', ['$scope','MedicalInstitutionalService', 
+  	function ($scope,MedicalInstitutionalService) {
+
+    $scope.medicalInstitutional = new MedicalInstitutionalService();
+  	$scope.listMedicalInstitutional = MedicalInstitutionalService.list();
+    $scope.cnpjInvalid=false;
+
+    //save
+  		$scope.save = function(){
+        if($scope.medicalInstitutional.id > 0){
+          $scope.update();
+        }else{
+          $scope.medicalInstitutional.$save(function(){
+          $scope.listMedicalInstitutional = MedicalInstitutionalService.list();
+          $scope.reset();
+        },function(response){
+              var errors =   response.data; 
+              if(response.data == '"CNPJ_EXISTS"'){                
+                $scope.cnpjInvalid=true;
+              }             
+          
+        });  
+        }
+  			
+  	};//end of save 
+  	
+
+    $scope.edit = function(med){
+      $scope.medicalInstitutional = med;
+    }
+
+   //update
+    $scope.update = function(){
+      $scope.medicalInstitutional.$update(function(){
+        $scope.listMedicalInstitutional = MedicalInstitutionalService.list();
+        $scope.reset();
+      });
+    };//end of update
+
+    //remove 
+    $scope.remove = function(medicalInstitutional){
+      medicalInstitutional.$remove({id : medicalInstitutional.id},function(res){
+        $scope.listMedicalInstitutional = MedicalInstitutionalService.list();
+        $scope.medicalInstitutional = new MedicalInstitutionalService();        
+      });            
+      };
+
+
+
+    $scope.reset = function(){
+  		$scope.medicalInstitutional = new MedicalInstitutionalService();
+      $scope.cnpjInvalid=false;
+  	};
+  
+  }]);
