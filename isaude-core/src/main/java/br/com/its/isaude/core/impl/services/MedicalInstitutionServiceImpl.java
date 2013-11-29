df66cddeeefc4d6c790581903b5a562ecf4b1b63
Camilo Lopes <camilo@camilolopes.com.br>
2013-11-29 20:59:28 -0200

    removed unimport classes

--- isaude-core/src/main/java/br/com/its/isaude/core/impl/services/MedicalInstitutionServiceImpl.java
+++ isaude-core/src/main/java/br/com/its/isaude/core/impl/services/MedicalInstitutionServiceImpl.java
@@ -1,12 +1,8 @@
 package br.com.its.isaude.core.impl.services;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 
 import br.com.its.isaude.core.exception.MedicalInstitutionException;
 import br.com.its.isaude.core.exception.enums.MessageResponseStatusEnum;
