291f36b600f181175faa2b87317cce3bfde8dc5f
Thiago <trgpwild@gmail.com>
2013-12-05 23:13:21 -0200

    FIX: US1 Update não retorna especialidades selecionadas

--- isaude-web/src/main/webapp/admin/app/scripts/controllers/doctorCtrl.js
+++ isaude-web/src/main/webapp/admin/app/scripts/controllers/doctorCtrl.js
@@ -62,28 +62,28 @@ angular.module('iSaudeAdminApp')
 			if ($scope.doctor.id > 0) {
 				$scope.update();
 			} else{
-			$scope.doctor.$create(function(success) {
-				$scope.reset();
-				$scope.list();
-				$scope.dataSuccess=true;
-			}, function(err) {
-				$scope.list();
-				var errors = err.data;
-				angular.forEach(errors, function(error) {
-					if (error.message == 'CRM_EXISTS') {
-						$scope.crmExists=true;
-					} else if (error.message == 'DOCTOR_EMAIL_EXISTS') {
-						$scope.doctorEmailExists=true;
-					}
+				$scope.doctor.$create(function(success) {
+					$scope.reset();
+					$scope.list();
+					$scope.dataSuccess=true;
+				}, function(err) {
+					$scope.list();
+					var errors = err.data;
+					angular.forEach(errors, function(error) {
+						if (error.message == 'CRM_EXISTS') {
+							$scope.crmExists=true;
+						} else if (error.message == 'DOCTOR_EMAIL_EXISTS') {
+							$scope.doctorEmailExists=true;
+						}
+					});
 				});
-			});
-		}//end of if/else
+			}//end of if/else
 
 		}
 
-		$scope.remove = function(doc){
-			$scope.doctorToRemove = doc;
-		}
+		$scope.remove = function(doctor){
+			$scope.doctorToRemove = doctor;
+		};
 
 		$scope.doRemove = function(){
 			$scope.doctorToRemove.$remove({id:$scope.doctorToRemove.id},function(res){
@@ -91,26 +91,50 @@ angular.module('iSaudeAdminApp')
 				$scope.doctor = new DoctorService();
 				$scope.dataRemoveSuccess = true;
 			})
-		}
+		};
 
-		$scope.edit = function(doc){
-			$scope.doctor = doc;
+		$scope.edit = function(doctor){
+
+			$scope.doctor = doctor;
 			$scope.disableCrm=true;
 			$scope.dataSuccess = false;
-		}
+
+			var specialitiesSel = angular.copy(doctor.especialidadeMedicas);
+			$scope.doctor.especialidadeMedicas = new Array();
+			angular.forEach($scope.medicalSpecialities, function(specialityOrig) {
+				angular.forEach(specialitiesSel, function(specialitySel) {
+					if (specialitySel.id === specialityOrig.id) {
+						$scope.doctor.especialidadeMedicas.push(specialityOrig);
+					}
+				});
+			});
+
+			var institutesSel = angular.copy(doctor.instituicaoMedicas);
+			$scope.doctor.instituicaoMedicas = new Array();
+			angular.forEach($scope.listMedicalInstitutional, function(instituteOrig) {
+				angular.forEach(institutesSel, function(instituteSel) {
+					if (instituteSel.id === instituteOrig.id) {
+						$scope.doctor.instituicaoMedicas.push(instituteOrig);
+					}
+				});
+			});
+
+		};
+
 		$scope.update = function(){
 			$scope.doctor.$update(function(){
 			$scope.doctors = DoctorService.list();
 			$scope.reset();
 			$scope.dataSuccess = true;
 			})
-		}
+		};
 
 		$scope.doctors = [];
 		$scope.fillSpecialities();
 		$scope.reset();
 		$scope.list();
 		$scope.getListMedicalInstitutional();
+	
 	}
 
 ]);
